09ee31ea2f6a108ec535b5cd3f1246c20cdbabcf
TheOnis <onisamuelmichael@gmail.com>
2023-09-27 17:12:06 +0100

    BE

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
-const api = axios.create({
-  baseURL: `${baseURL}`,
-});
-
-export default api;
