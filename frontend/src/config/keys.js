a9b6e86f50d3da224865d32c76ed59a0ba716b02
TheOnis <onisamuelmichael@gmail.com>
2023-09-27 20:30:43 +0100

    progress

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -1,6 +1,6 @@
 const keys = {
-  REACT_APP_API_KEY: "",
-  REACT_APP_API_KEY_VIRUS_TOTAL: "",
+  REACT_APP_API_KEY: "4810fec8c6ef47dc872e4683cc8930e9",
+  REACT_APP_API_KEY_VIRUS_TOTAL: "1eb413edca7d83d8b7789cebb7bc6cb7276abf44fe0b592cf18b04aeeaea48bc",
 };
 
 export default keys;
