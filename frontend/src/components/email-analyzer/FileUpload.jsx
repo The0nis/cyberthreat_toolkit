63fe1e0f469b5622615aefe4046ed01a2a5ca1bc
TheOnis <onisamuelmichael@gmail.com>
2023-10-03 22:43:27 +0100

    in progress

--- frontend/src/components/email-analyzer/FileUpload.jsx
+++ frontend/src/components/email-analyzer/FileUpload.jsx
@@ -9,6 +9,7 @@ import Introduction from "../Introduction";
 import Result from "./Result";
 
 import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
+import keys from "../../config/keys";
 
 export default function FileUpload(props) {
   const theme = useTheme();
@@ -81,31 +82,97 @@ export default function FileUpload(props) {
     setShowResult(true);
   };
 
-  function uploadFiles(file) {
-    const apiUrl = "https://eml-analyzer.herokuapp.com/api/analyze/file";
+  // function uploadFiles(file) {
+  //   const apiUrl = "https://eml-analyzer.herokuapp.com/api/analyze/file";
 
-    const formData = new FormData();
-    formData.append("file", file, file.name);
+  //   const formData = new FormData();
+  //   formData.append("file", file, file.name);
 
-    const config = {
-      headers: {
-        accept: "application/json",
-        "Content-Type": "multipart/form-data",
-      },
-    };
+  //   const config = {
+  //     headers: {
+  //       accept: "application/json",
+  //       "Content-Type": "multipart/form-data",
+  //     },
+  //   };
 
-    api
-      .post(apiUrl, formData, config)
-      .then((response) => {
-        const result = response.data;
-        setFile(result);
-        handleShowResult();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  //   api
+  //     .post(apiUrl, formData, config)
+  //     .then((response) => {
+  //       const result = response.data;
+  //       setFile(result);
+  //       handleShowResult();
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+
+  async function uploadFiles(file) {
+    if (file) {
+      try {
+        // Calculate the SHA hash of the file
+        const shaHash = await calculateSHA(file);
+  
+        // Check if SHA hash was calculated successfully
+        if (!shaHash) {
+          console.error('Error calculating SHA hash');
+          return;
+        }
+  
+        // Send the SHA hash to the VirusTotal endpoint
+        const apiUrl = 'https://www.virustotal.com/api/v3/files/sha256/' + shaHash;
+  
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+          headers: {
+            'x-apikey': keys.REACT_APP_API_KEY_VIRUS_TOTAL, // Replace with your VirusTotal API key
+          },
+        });
+  
+        if (response.ok) {
+          const result = await response.json();
+          // Handle the result or pass it to a callback function
+          setFile(result);
+          handleShowResult();
+        } else {
+          console.error('Error fetching data from VirusTotal:', response.statusText);
+          // Handle the error as needed
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+        // Handle the error as needed
+      }
+    } else {
+      alert('Please select a file to upload.');
+    }
   }
-
+  
+  async function calculateSHA(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onload = function (event) {
+        const arrayBuffer = event.target.result;
+        const crypto = window.crypto || window.msCrypto; // Browser compatibility
+  
+        if (!crypto) {
+          reject('Crypto API not available');
+          return;
+        }
+  
+        crypto.subtle.digest('SHA-256', arrayBuffer).then((hashBuffer) => {
+          const hashArray = Array.from(new Uint8Array(hashBuffer));
+          const shaHash = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+          resolve(shaHash);
+        }).catch((error) => {
+          reject(error);
+        });
+      };
+  
+      reader.readAsArrayBuffer(file);
+    });
+  }
+  
   return (
     <div className="drop">
       <br />
