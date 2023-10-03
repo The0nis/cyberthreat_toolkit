0382acf34b315b749f6114c77f9a933fd4726631
TheOnis <onisamuelmichael@gmail.com>
2023-10-03 19:43:09 +0100

    ap fixi integrated

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const app = express();
 const multer = require("multer"); // Require the multer library to handle file uploads
 const upload = multer({ dest: "uploads/" });
 
-
 // Allow CORS from any origin
 const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
@@ -18,7 +17,7 @@ const { dummyNewsfeedData } = require("./dummyData");
 
 // Simulated module settings (replace with your actual data)
 const moduleSettings = {
-  "Newsfeed": {
+  Newsfeed: {
     enabled: true,
     description: "Newsfeed module description",
   },
@@ -46,78 +45,77 @@ const moduleSettings = {
     enabled: false,
     description: "CVSS Calculator module description",
   },
-  "Rules": {
+  Rules: {
     enabled: true,
     description: "Rules module description",
   },
-  "Settings": {
+  Settings: {
     enabled: true,
     description: "Settings module description",
   },
 };
 
 const generalSettings = {
-    font: "Arial",
-    // Add other general settings properties as needed
-  };
+  font: "Arial",
+  // Add other general settings properties as needed
+};
 
 // Simulated API key state (replace with your actual data)
 const apiKeysState = {
-    is_active: true, // Change this value based on your API key state
-  };
-  
-  // Define a route to get module settings
-  app.get("/api/settings/modules/", (req, res) => {
-    res.json(moduleSettings);
-  });
-  
-  // Define a new route to get the state of API keys
-  app.get("/api/apikeys/is_active", (req, res) => {
-    // Respond with the state of API keys
-    res.json(apiKeysState);
-  });
+  is_active: true, // Change this value based on your API key state
+};
+
+// Define a route to get module settings
+app.get("/api/settings/modules/", (req, res) => {
+  res.json(moduleSettings);
+});
+
+// Define a new route to get the state of API keys
+app.get("/api/apikeys/is_active", (req, res) => {
+  // Respond with the state of API keys
+  res.json(apiKeysState);
+});
 
 // Define a new route to get a dummy list of RSS feeds
 app.get("/api/settings/modules/newsfeed/", (req, res) => {
-    // Dummy RSS feed data
-    const dummyFeeds = [
-      {
-        name: "Feed 1",
-        url: "https://example.com/feed1",
-      },
-      {
-        name: "Feed 2",
-        url: "https://example.com/feed2",
-      },
-      {
-        name: "Feed 3",
-        url: "https://example.com/feed3",
-      },
-      // Add more feeds as needed
-    ];
-  
-    res.json(dummyFeeds);
-  });
+  // Dummy RSS feed data
+  const dummyFeeds = [
+    {
+      name: "Feed 1",
+      url: "https://example.com/feed1",
+    },
+    {
+      name: "Feed 2",
+      url: "https://example.com/feed2",
+    },
+    {
+      name: "Feed 3",
+      url: "https://example.com/feed3",
+    },
+    // Add more feeds as needed
+  ];
 
-  app.get("/api/settings/general/", (req, res) => {
-    // Respond with the general settings data
-    res.json([generalSettings]);
-  });
+  res.json(dummyFeeds);
+});
 
-  app.get("/api/newsfeed", (req, res) => {
-    // Respond with the dummy newsfeed data
-    res.json(dummyNewsfeedData);
-  });
+app.get("/api/settings/general/", (req, res) => {
+  // Respond with the general settings data
+  res.json([generalSettings]);
+});
 
+app.get("/api/newsfeed", (req, res) => {
+  // Respond with the dummy newsfeed data
+  res.json(dummyNewsfeedData);
+});
 
 // Define a route to handle file uploads (proxy)
-app.options('/api/mailanalyzer', cors(corsOptions)); // Handle CORS preflight request
-app.post('/api/mailanalyzer', cors(corsOptions), async (req, res) => {
-  const apiUrl = 'https://eml-analyzer.herokuapp.com/api/analyze/file';
+app.options("/api/mailanalyzer", cors(corsOptions)); // Handle CORS preflight request
+app.post("/api/mailanalyzer", cors(corsOptions), async (req, res) => {
+  const apiUrl = "https://eml-analyzer.herokuapp.com/api/analyze/file";
 
   try {
     const response = await fetch(apiUrl, {
-      method: 'POST',
+      method: "POST",
       body: req.body, // Forward the request body from your frontend
       headers: req.headers, // Forward headers, including any authentication headers
     });
@@ -126,33 +124,31 @@ app.post('/api/mailanalyzer', cors(corsOptions), async (req, res) => {
       const data = await response.json();
       res.json(data);
     } else {
-      console.error('Error in external API:', response.statusText);
-      res.status(response.status).json({ error: 'Error in external API' });
+      console.error("Error in external API:", response.statusText);
+      res.status(response.status).json({ error: "Error in external API" });
     }
   } catch (error) {
-    console.error('An error occurred:', error);
-    res.status(500).json({ error: 'An error occurred while processing the request' });
+    console.error("An error occurred:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request" });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/api/settings/general/darkmode", (req, res) => {
+  const { darkmode } = req.query;
+
+  // You can save the dark mode state in your server or toggle it based on your requirements.
+  // For this example, we'll simply respond with a JSON message indicating the current mode.
+  if (darkmode === "true") {
+    // Save or toggle dark mode to "true"
+    res.json({ message: "Dark mode is enabled." });
+  } else {
+    // Save or toggle dark mode to "false"
+    res.json({ message: "Dark mode is disabled." });
+  }
 });
 
-  app.get("/api/settings/general/darkmode", (req, res) => {
-    const { darkmode } = req.query;
-  
-    // You can save the dark mode state in your server or toggle it based on your requirements.
-    // For this example, we'll simply respond with a JSON message indicating the current mode.
-    if (darkmode === "true") {
-      // Save or toggle dark mode to "true"
-      res.json({ message: "Dark mode is enabled." });
-    } else {
-      // Save or toggle dark mode to "false"
-      res.json({ message: "Dark mode is disabled." });
-    }
-  });
-  
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
